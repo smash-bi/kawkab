17883cf8d0e023ae3085f9fbc9767d00b7408bd7
Sajjad Rizvi <sajjad.rizvi5@gmail.com>
2020-09-03 23:09:23 -0400

    removed duplicate stats collector

--- src/main/java/kawkab/fs/core/PartitionedBufferedCache.java
+++ src/main/java/kawkab/fs/core/PartitionedBufferedCache.java
@@ -61,9 +61,6 @@ public class PartitionedBufferedCache extends Cache {
 		pinLock = new KeyedLock<>();
 
 		//runEvictor();
-
-		runStatsCollector();
-
 	}
 	
 	public static PartitionedBufferedCache instance() {
@@ -394,7 +391,7 @@ public class PartitionedBufferedCache extends Cache {
 					//accm.put((int) ((clock.currentTime() - startT) / 1000.0), occupancy);
 
 
-					//System.out.println(String.format("%d: %.2f, %.2f, %.2f, %.2f\n",++n, cacheOcc, lsOcc, canEvict, gsQlen));
+					System.out.println(String.format("%d: %.2f, %.2f, %.2f, %.2f\n",++n, cacheOcc, lsOcc, canEvict, gsQlen));
 					writer.write(String.format("%.2f, %.2f, %.2f, %.2f\n",cacheOcc, lsOcc, canEvict, gsQlen));
 
 					try {
